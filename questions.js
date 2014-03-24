3a8a36dd8201185824d8e37373e0eab731b877aa
Jeremy Robson <jeremy.robson@gmail.com>
2014-03-23 22:09:59 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,41 @@
+var questions = [];
+questions[0] = {
+    "text": "What does a dog say?",
+    "answer": "woof"
+};
+questions[1] = {
+    "text": "What does a cat say?",
+    "answer": "meow"
+};
+questions[2] = {
+    "text": "What does a cow say?",
+    "answer": "moo"
+};
+questions[3] = {
+    "text": "What does a pig say?",
+    "answer": "oink"
+};
+questions[4] = {
+    "text": "What does a mouse say?",
+    "answer": "squeak"
+};
+questions[5] = {
+    "text": "What does a horse say?",
+    "answer": "nayyy"
+};
+questions[6] = {
+    "text": "What does a sheep say?",
+    "answer": "baaaa"
+};
+questions[7] = {
+    "text": "What does a bird say?",
+    "answer": "tweet"
+};
+questions[8] = {
+    "text": "What does a robot say?",
+    "answer": "beepboopbeepboop"
+};
+questions[9] = {
+    "text": "What does a frog say?",
+    "answer": "ribbit"
+};
